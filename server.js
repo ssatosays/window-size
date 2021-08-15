97c4071b985b8ed30c342638f77c6fe2f86077e9
ssatosays <ssatosays@gmail.com>
2021-08-15 11:50:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const app = express()
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(basicAuth({
-  challenge: true,
-  unauthorizedResponse: () => {
-    return "Unauthorized"
-  },
-  authorizer: (username, password) => {
-    const correctUserName = '1234'
-    const correctPassword = '1234'
-    const userMatch = basicAuth.safeCompare(username, correctUserName)
-    const passMatch = basicAuth.safeCompare(password, correctPassword)
-
-    return userMatch & passMatch
-  },
-}))
+// app.use(basicAuth({
+//   challenge: true,
+//   unauthorizedResponse: () => {
+//     return "Unauthorized"
+//   },
+//   authorizer: (username, password) => {
+//     const correctUserName = '1234'
+//     const correctPassword = '1234'
+//     const userMatch = basicAuth.safeCompare(username, correctUserName)
+//     const passMatch = basicAuth.safeCompare(password, correctPassword)
+// 
+//     return userMatch & passMatch
+//   },
+// }))
 
 app.use(express.static(__dirname + '/docs'))
  
